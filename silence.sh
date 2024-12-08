affdddae0a35999c705bed0ece979c837f4518b2
Simon Walker <github@stwalkerster.co.uk>
2024-12-08 17:51:07 +0000

    Update more configuration

--- silence.sh
+++ silence.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /run/secrets/*
 
-exec ffmpeg -re -ac 1 -f lavfi -i anullsrc -acodec libmp3lame -ab 32k -ac 1 -content_type audio/mpeg -f mp3 icecast://source:${STREAM_PASSWORD}@icecast:8000/fallback
+exec ffmpeg -re -ac 1 -f lavfi -i anullsrc -acodec libmp3lame -ab 32k -ac 1 -content_type audio/mpeg -f mp3 icecast://source:${STREAM_PASSWORD}@${STREAM_HOST}:8000/${STREAM_NAME}
